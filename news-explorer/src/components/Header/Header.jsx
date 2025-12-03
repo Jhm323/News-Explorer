a690f7aceb0e5f30a7e1a30d7fcd741c12073e1c
holden <james.holden.moore@gmail.com>
2025-12-03 13:54:06 -0500

    Header component and styles

--- news-explorer/src/components/Header/Header.jsx
+++ news-explorer/src/components/Header/Header.jsx
@@ -1,13 +1,20 @@
-import React from "react";
-import Navigation from "../Navigation/Navigation";
 import "./Header.css";
 
 function Header() {
   return (
     <header className="header">
-      <Navigation />
+      <div className="header__content">
+        <p className="header__logo">NewsExplorer</p>
+        <div className="header__right">
+          <nav className="header__nav">
+            <a href="#" className="header__nav-link" id="">
+              Home
+            </a>
+          </nav>
+          <button className="header__signin-btn">Signin</button>
+        </div>
+      </div>
     </header>
   );
 }
-
 export default Header;
