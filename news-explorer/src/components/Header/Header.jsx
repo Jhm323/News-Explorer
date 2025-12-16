5107c5afd8adeb6b6ba7b1ec5008a6ee550fe942
holden <james.holden.moore@gmail.com>
2025-12-15 21:35:46 -0500

    refactoring header-menu/modal click

--- news-explorer/src/components/Header/Header.jsx
+++ news-explorer/src/components/Header/Header.jsx
@@ -38,42 +38,12 @@ function Header({ isLoggedIn, onLoginClick, onRegisterClick, onLogoutClick }) {
         <button
           type="button"
           className="header__mobile-menu"
-          aria-label="Toggle menu"
-          aria-expanded={menuOpen}
-          aria-controls="header-side-menu"
-          onClick={() => setMenuOpen(!menuOpen)}
+          aria-label="Sign in"
+          onClick={onLoginClick}
         >
           <span className="header__mobile-menu-icon"></span>
         </button>
       </div>
-
-      {menuOpen && (
-        <nav id="header-side-menu" className="header__side-menu">
-          <Navigation className="header__side-menu__nav" />
-          <div className="header__auth">
-            {isLoggedIn ? (
-              <button className="header__logout-button" onClick={onLogoutClick}>
-                Logout
-              </button>
-            ) : (
-              <>
-                <button
-                  className="header__signin-button"
-                  onClick={onLoginClick}
-                >
-                  Sign In
-                </button>
-                <button
-                  className="header__signup-button"
-                  onClick={onRegisterClick}
-                >
-                  Sign Up
-                </button>
-              </>
-            )}
-          </div>
-        </nav>
-      )}
     </header>
   );
 }
