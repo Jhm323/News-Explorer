7103a11a59b3146029f9749ccf4ba9eaef4fb552
holden <james.holden.moore@gmail.com>
2025-12-18 18:24:31 -0500

    double checking button styles and transations on all interactive elements

--- news-explorer/src/utils/constants.js
+++ news-explorer/src/utils/constants.js
@@ -1,60 +1,63 @@
 export const SAMPLE_ARTICLES = [
   {
     source: { name: "BBC News" },
-    title: "Endangered Rhino Born",
+    title: "Endangered red panda arrives at Zoo",
     description: "Critically endangered rhino born at farm",
     publishedAt: "2024-01-15T10:30:00Z",
-    urlToImage: "https://www.bbc.com/news/videos/cdd52y8nm80o",
-    url: "https://www.bbc.com/news/videos/cdd52y8nm80o",
+    urlToImage:
+      "https://ichef.bbci.co.uk/news/800/cpsprodpb/48fa/live/3ef59400-bf17-11f0-aa9c-2dd9cb62df51.jpg.webp",
+    url: "https://www.bbc.com/news/articles/cgjdx176vevo",
   },
   {
     source: { name: "NBC News" },
-    title: "First Giant Panda Cub Born",
+    title: "First Giant Panda Cub Born, Zoo",
     description:
       "Satrio Wiratama, nicknamed Rio, was born Nov. 27 at a conservation park in West Java province. He is stable and showing healthy early signs, the park said.",
     publishedAt: "2024-01-15T10:30:00Z",
     urlToImage:
-      "https://www.nbcnews.com/world/asia/first-giant-panda-cub-born-indonesia-squeals-squirms-park-video-rcna247957",
+      "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1000w,f_auto,q_auto:best/rockcms/2025-12/251208-giant-panda-cub-rs-e5893b.jpg",
     url: "https://www.nbcnews.com/world/asia/first-giant-panda-cub-born-indonesia-squeals-squirms-park-video-rcna247957",
   },
   {
     source: { name: "NBC News" },
-    title: "Bear Takes Up Residence in Home's Crawlspace",
+    title:
+      "Jane Goodall, Renowned Chimpanzee Researcher and Animal Advocate, Dies at 91",
     description:
-      "The bear that moved into a home’s crawlspace more than a week ago in Altadena is not budging despite numerous attempts by the homeowners to keep him out.",
+      "Jane Goodall, renowned chimpanzee researcher and animal advocate, dies at 91",
     publishedAt: "2024-01-14T15:45:00Z",
     urlToImage:
-      "https://www.nbcnews.com/video/bear-living-under-california-home-shows-no-signs-of-leaving-253414469771",
-    url: "https://www.nbcnews.com/video/bear-living-under-california-home-shows-no-signs-of-leaving-253414469771",
+      "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-560w,f_auto,q_auto:best/rockcms/2025-10/251001-jane-goddall-early-gk-209491.jpg",
+    url: "https://www.nbcnews.com/news/obituaries/dr-jane-goodall-legendary-animal-rights-advocate-dies-91-rcna235001",
   },
   {
     source: { name: "NBC News" },
-    title: "Want to clone your dog? Don't expect an exact replica",
+    title:
+      "Should Scientest be allowed to edit the genes of wild animals?, Zoo",
     description:
-      'Arthur Murray says his dogs Stella and Mella are "100% physically identical." Their personalities? Less similar.',
+      "The world’s largest network of environmental groups approved the exploration of genetic engineering tools to aid in the preservation of animal species.",
     publishedAt: "2024-01-13T08:20:00Z",
     urlToImage:
-      "https://www.nbcnews.com/science/science-news/want-clone-dog-dont-expect-exact-replica-rcna246942",
-    url: "https://www.nbcnews.com/science/science-news/want-clone-dog-dont-expect-exact-replica-rcna246942",
+      "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1000w,f_auto,q_auto:best/rockcms/2025-04/250407-dire-wolves-young-ac-1057p-949351.jpg",
+    url: "https://www.nbcnews.com/science/science-news/animals-genetic-engineering-iucn-conservation-groups-vote-yes-rcna235522",
   },
   {
     source: { name: "Smithsonian's National Zoo" },
-    title: "For Migratory Species, Fences Don’t Make Good Neighbors ›",
+    title: "For Migratory Species, Fences Don’t Make Good Neighbors, Zoo ›",
     description:
       "Fences are creating serious problems for wildebeest herds, says a Smithsonian ecologist studying megafauna migrations in Africa.",
     publishedAt: "2024-01-12T12:15:00Z",
     urlToImage:
-      "https://nationalzoo.si.edu/conservation-ecology-center/news/migratory-species-fences-dont-make-good-neighbors",
-    url: "https://nationalzoo.si.edu/conservation-ecology-center/news/migratory-species-fences-dont-make-good-neighbors",
+      "https://nationalzoo.si.edu/sites/default/files/styles/wide/public/2025-12/DSCF6111_Landscape_Fence_and_Wildebeest_David%20Chancellor%281%29.jpg?h=37bc4dc1&itok=X6X29Gng",
+    url: "https://nationalzoo.si.edu/sites/default/files/styles/wide/public/2025-12/DSCF6111_Landscape_Fence_and_Wildebeest_David%20Chancellor%281%29.jpg?h=37bc4dc1&itok=X6X29Gng",
   },
   {
     source: { name: "Smithsonian's National Zoo" },
-    title: "Asian Elephant Program Media Resources ›",
+    title: "Asian Elephant Program Media Resources, Zoo ›",
     description:
       "Find comprehensive resources for media on the Asian elephant program at the Smithsonian's National Zoo and Conservation Biology Institute.",
     publishedAt: "2024-01-11T09:00:00Z",
     urlToImage:
-      "https://nationalzoo.si.edu/news/asian-elephant-program-media-resources",
-    url: "https://nationalzoo.si.edu/news/asian-elephant-program-media-resources",
+      "https://nationalzoo.si.edu/sites/default/files/styles/wide/public/2025-11/20180628-084SkipBrown-crop.png?h=c9a3a702&itok=cmArVrne",
+    url: "https://nationalzoo.si.edu/animals/asian-elephant",
   },
 ];
