78451b46073f8ccba7136dd5d1ebdd4be864375f
holden <james.holden.moore@gmail.com>
2025-12-24 13:16:15 -0500

    contitional nav link for SavedNews @ 768px

--- news-explorer/src/components/Navigation/Navigation.jsx
+++ news-explorer/src/components/Navigation/Navigation.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 import "./Navigation.css";
 
 function Navigation({ className = "" }) {
+  const { isLoggedIn } = useContext(AuthContext);
+
   return (
     <nav className={`navigation ${className}`.trim()}>
       <ul className="navigation__list">
@@ -18,18 +21,20 @@ function Navigation({ className = "" }) {
             Home
           </NavLink>
         </li>
-        <li className="navigation__item navigation__item_saved">
-          <NavLink
-            to="/saved-news"
-            className={({ isActive }) =>
-              isActive
-                ? "navigation__link navigation__link_active"
-                : "navigation__link"
-            }
-          >
-            Saved articles
-          </NavLink>
-        </li>
+        {isLoggedIn && (
+          <li className="navigation__item navigation__item_saved">
+            <NavLink
+              to="/saved-news"
+              className={({ isActive }) =>
+                isActive
+                  ? "navigation__link navigation__link_active"
+                  : "navigation__link"
+              }
+            >
+              Saved articles
+            </NavLink>
+          </li>
+        )}
       </ul>
     </nav>
   );
