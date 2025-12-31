b79ed7e9172d8fe765c4004151faf319b8b63add
holden <james.holden.moore@gmail.com>
2025-12-31 11:25:28 -0500

    gitHub pages deployment

--- news-explorer/vite.config.js
+++ news-explorer/vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/news-explorer-frontend/",
   server: {
-    port: 3001,
+    base: "/news-explorer-frontend/",
   },
 });
