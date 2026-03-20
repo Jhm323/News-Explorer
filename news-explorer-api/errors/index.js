2947789ddc5b2dfb1e114f002767034ffa440de5
holden <james.holden.moore@gmail.com>
2026-03-20 09:37:24 -0400

    fix require paths to class errors in index.js

--- news-explorer-api/errors/index.js
+++ news-explorer-api/errors/index.js
@@ -1,8 +1,8 @@
-const BadRequestError = require("./BadRequestError");
-const UnauthorizedError = require("./UnauthorizedError");
-const ForbiddenError = require("./ForbiddenError");
-const NotFoundError = require("./NotFoundError");
-const ConflictError = require("./ConflictError");
+const BadRequestError = require("./classes/BadRequestError");
+const UnauthorizedError = require("./classes/UnauthorizedError");
+const ForbiddenError = require("./classes/ForbiddenError");
+const NotFoundError = require("./classes/NotFoundError");
+const ConflictError = require("./classes/ConflictError");
 
 module.exports = {
   BadRequestError,
